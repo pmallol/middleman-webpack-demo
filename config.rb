3740596f88a5b51314a87d648afdb2eb7ea714d3
pmallol <pmallol@manas.tech>
2020-06-26 15:31:56 -0300

    Configure Middleman's build enviroment

--- config.rb
+++ config.rb
@@ -57,7 +57,9 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  config[:css_dir] = ""
+  config[:js_dir] = ""
+  # activate :minify_css
+  # activate :minify_javascript
+end
